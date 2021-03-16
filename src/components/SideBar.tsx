84fbea8684431626a3d994a18bfeae597acb8697
rellyso <rellysondouglas2015@gmail.com>
2021-03-16 10:46:31 -0300

    building Sidebar: added interface and JSX elements

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,16 @@
-export function SideBar() {
-  // Complete aqui
+interface SidebarProps {
+  title: string | JSX.Element;
+  children: JSX.Element[];
+}
+
+export function SideBar({ children, title }: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>{title}</span>
+
+      <div className="buttons-container">
+        {children}
+      </div>
+    </nav>
+  )
 }
