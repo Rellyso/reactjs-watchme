012a1bde628c971fd40a54efc6bef2ae8259b948
rellyso <rellysondouglas2015@gmail.com>
2021-03-16 10:48:32 -0300

    building: added Category container & her interface

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,20 @@
-export function Content() {
-  // Complete aqui
+interface ContentProps {
+  categoryName: string;
+  children: JSX.Element[];
+}
+
+export function Content({ children, categoryName }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {categoryName}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
 }
